850852f63c2de8534bc2762f0ac768589d6e2479
David Halison <halison700@gmail.com>
2022-09-18 20:32:13 -0300

    Alterado JS e CSS da aplicação

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,8 +7,17 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
+# application.js.erb, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( coins.css
+                                                  welcome.css
+                                                  mining_types.css
+                                                  scaffolds.css
+                                                  )
+Rails.application.config.assets.precompile += %w( coins.js
+                                                  welcome.js
+                                                  mining_types.js
+                                                  cable.js
+                                                  )
