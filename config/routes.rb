019f00909f30a0a9177f75cfde1ff848386e01a3
david halison <halison700@gmail.com>
2022-09-23 23:52:37 -0300

    ajustes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   resources :mining_types
+  #get '/mining_types', to: 'mining_types#index'
+
   get 'welcome/index'
   #get '/inicio', to: 'welcome#index'
 
   resources :coins
   #get '/coins', to: 'coins#index'
-  
-  root to: 'welcome#index'
-  
+    root to: 'welcome#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
