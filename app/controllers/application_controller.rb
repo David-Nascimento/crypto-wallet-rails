3858403f2de9a9fde1215329e280ac21d96275d2
david halison <halison700@gmail.com>
2022-09-24 20:02:36 -0300

    Ajustes no I18n internalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :set_locale
 
+  private
   def set_locale
-    session[:locale] = params[:locale] if params[:locale].present?
-    cookies[:locale]
-    if cookies[:locale].present?
-      I18n.locale = session[:locale] || I18n.default_locale
-    end
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge options
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 
   resources :coins
   #get '/coins', to: 'coins#index'
-    root to: 'welcome#index'
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    root 'welcome#index'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
